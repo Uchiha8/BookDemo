2fc7b23f3d07f218609617d1331dd48c48ffee0d
Uchiha8 <mnajimov.unlimit@gmail.com>
2023-11-06 02:39:05 +0500

    initial commit

---
+++ src/main/java/com/example/bookdemo/domain/BookController.java
@@ -0,0 +1,43 @@
+package com.example.bookdemo.domain;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class BookController {
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping("/createBook")
+    public ResponseEntity<Book> save(@RequestBody Book book) {
+        return ResponseEntity.ok(bookService.save(book));
+    }
+
+    @PutMapping("/updateBook/id")
+    public ResponseEntity<Book> update(@RequestParam Long id, @RequestBody Book book) {
+        return ResponseEntity.ok(bookService.update(id, book));
+    }
+
+    @GetMapping("/readBook/id")
+    public ResponseEntity<Book> read(@RequestParam Long id) {
+        return ResponseEntity.ok(bookService.getById(id));
+    }
+
+    @GetMapping("/readAll")
+    public ResponseEntity<List<Book>> readAll() {
+        List<Book> bookList = bookService.getAll();
+        return ResponseEntity.ok(bookList);
+    }
+
+    @DeleteMapping("/delete/id")
+    public ResponseEntity<String> delete(@RequestParam Long id) {
+        bookService.deleteById(id);
+        return ResponseEntity.ok("Data has been deleted");
+    }
+}
---
+++ src/main/java/com/example/bookdemo/domain/BookService.java
@@ -0,0 +1,45 @@
+package com.example.bookdemo.domain;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public Book save(Book book) {
+        bookRepository.save(book);
+        return book;
+    }
+
+    public Book update(Long id, Book book) {
+        Book update_book = bookRepository.getBookById(id);
+        update_book.setName(book.getName());
+        update_book.setAuthor(book.getAuthor());
+        update_book.setDescription(book.getDescription());
+        update_book.setId(book.getId());
+        bookRepository.save(update_book);
+        return update_book;
+    }
+
+    public Book getById(Long id) {
+        return bookRepository.getBookById(id);
+    }
+
+    public List<Book> getAll() {
+        List<Book> bookList = bookRepository.findAll();
+        return bookList;
+    }
+
+    public void deleteById(Long id) {
+        bookRepository.deleteById(id);
+    }
+
+}
+
